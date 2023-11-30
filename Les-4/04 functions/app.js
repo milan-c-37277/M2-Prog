fddb16890d6cb65d5f41007980c1ce358871b942
Milan Carati <37277@ma-web.nl>
2023-11-30 14:05:18 +0100

    Les 4 gemaakt

---
+++ Les-4/04 functions/app.js
@@ -0,0 +1,11 @@
+function SuperMooieGlobalFunction()
+{
+    console.log("Ik ben global");
+    console.log("Dus je mag mij overal aanroepen");
+}
+
+
+SuperMooieGlobalFunction();
+superMooieGlobalFunction();
+superMooieGlobalFunction();
+superMooieGlobalFunction();
