cc47f44623d73f7411527f5c21273442bd86b15b
Milan Carati <37277@ma-web.nl>
2023-12-19 14:19:33 +0100

    Les 6 - 02 (kortere versie dan Les 6 - 1)

---
+++ Les-6/02-functions/app.js
@@ -0,0 +1,13 @@
+class App
+{
+    runApplication()
+    {
+        let uiButton = document.getElementById('clickable');
+        uiButton.addEventListener("click",(e)=>
+        {
+            console.log("Click!")
+        })
+}
+
+let app = new App();
+app.runApplication();
